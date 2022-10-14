605757e2bda8c626e8156fd0600a3450fa124318
ggabrielaoliveira <gabi.oliveiraa@yahoo.com>
2022-10-13 23:52:20 -0300

    consegui menos o header

--- template/src/App.js
+++ template/src/App.js
@@ -1,14 +1,19 @@
 import React from "react";
 import "./App.css";
-import logo from "./img/logo.png";
+import Garagem from "./componentes/Garagem";
+import Footer from "./componentes/Footer";
+import Header from "./componentes/Header";
+
 
 export default function App() {
   
 
   return (
     <div className="App">
-      
+      <Header/>
      <h1>Aula - Componentes React</h1>
+     <Garagem/>
+     <Footer/>
     </div>
   );
 }
